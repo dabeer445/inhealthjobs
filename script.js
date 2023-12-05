fbf1da1d8c8332d08b5d64ee61d291b3b736962e
Dabeer <dabeerqf@gmail.com>
2023-12-06 01:42:09 +0500

    added feat

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// <script data-version="production" src="https://cdn.jsdelivr.net/gh/zacsanter/inhealthjobs@main/inj--script-demo5.js" id="vfassistant"></script>
+
 const typingIndicator = document.getElementById("typing-indicator");
 const uniqueId = generateUniqueId();
 const voiceflowRuntime = "general-runtime.voiceflow.com";
@@ -17,6 +19,10 @@ const savedMessages = localStorage.getItem("messages");
 const chatContainer = document.getElementById("chat-container");
 const restartButton = document.getElementById("restart-button");
 
+let uppy;
+const assistantTag = "InHealth Jobs",
+  userTag = "You";
+
 function displayResponse(response) {
   setTimeout(() => {
     if (response) {
@@ -24,23 +30,21 @@ function displayResponse(response) {
         if (item.type === "speak" || item.type === "text") {
           console.info("Speak/Text Step");
 
-          const taglineElement = document.createElement("div");
-          taglineElement.classList.add("assistanttagline");
-          taglineElement.textContent = "InHealth Jobs";
+          // const taglineElement = document.createElement("div");
+          // taglineElement.classList.add("assistanttagline");
+          // taglineElement.textContent = "InHealth Jobs";
 
+          // chatWindow.appendChild(assistantTagLine);
 
-          chatWindow.appendChild(taglineElement);
+          // const assistantWrapper = document.createElement("div");
+          // assistantWrapper.classList.add("assistantwrapper");
 
-          const assistantWrapper = document.createElement("div");
-          assistantWrapper.classList.add("assistantwrapper");
-
-          const assistantImage = document.createElement("div");
-          assistantImage.classList.add("assistantimage");
-          assistantWrapper.appendChild(assistantImage);
+          // const assistantImage = document.createElement("div");
+          // assistantImage.classList.add("assistantimage");
+          // assistantWrapper.appendChild(assistantImage);
 
           const messageElement = document.createElement("div");
           messageElement.classList.add("message", "assistant");
-          assistantWrapper.appendChild(messageElement);
 
           const paragraphs = item.payload.message.split("\n\n");
           const wrappedMessage = paragraphs
@@ -48,8 +52,11 @@ function displayResponse(response) {
             .join("");
 
           messageElement.innerHTML = wrappedMessage;
-          chatWindow.appendChild(assistantWrapper);
 
+          // assistantWrapper.appendChild(messageElement);
+          // chatWindow.appendChild(assistantWrapper);
+
+          addAssistantMsg(messageElement);
         } else if (item.type === "choice") {
           const buttonContainer = document.createElement("div");
           buttonContainer.classList.add("buttoncontainer");
@@ -65,6 +72,7 @@ function displayResponse(response) {
             buttonContainer.appendChild(buttonElement);
           });
           chatWindow.appendChild(buttonContainer);
+          localStorage.setItem("messages", chatWindow.innerHTML);
         } else if (item.type === "visual") {
           console.info("Image Step");
 
@@ -72,9 +80,56 @@ function displayResponse(response) {
           imageElement.src = item.payload.image;
           imageElement.alt = "Assistant Image";
           imageElement.style.width = "100%";
-          chatWindow.appendChild(imageElement);
+
+          addAssistantMsg(imageElement);
+
+          // chatWindow.appendChild(imageElement);
+        } else if (item.type === "upload_resume") {
+          const uppyElement = document.createElement("div");
+          uppyElement.id = "uppyElement";
+          uppyElement.style.flex = 1;
+          // uppyElement.classList.add("assistantwrapper");
+
+          // chatWindow.appendChild(assistantTagLine);
+          // chatWindow.appendChild(uppyElement);
+          addAssistantMsg(uppyElement);
+
+          uppy = new Uppy.Uppy({
+            autoProceed: true,
+            allowMultipleUploadBatches: false,
+            debug: false,
+            restrictions: {
+              allowedFileTypes: [".pdf"],
+            },
+          });
+          uppy
+            .use(Uppy.Dashboard, {
+              target: "#uppyElement",
+              inline: true,
+              proudlyDisplayPoweredByUppy: false,
+            })
+            .use(Uppy.XHRUpload, {
+              endpoint: "https://inj-cv-parser.adabeer445.repl.co/extract_text",
+              fieldName: "file",
+            });
+          uppy.on("upload-success", async (file, response) => {
+            // Parse the response body as JSON
+            console.log(file);
+            console.log(response);
+            await updateVariable(
+              "resume",
+              response.body.text.replaceAll("\n", " ")
+            );
+            uppy.clearUploadedFiles();
+            uppy.close();
+            uppyElement.parentElement.parentElement.remove();
+            interact({ type: "done", payload: null });
+            // console.log(uppy);
+          });
+        } else if (item.type === "user_form") {
+          addAssistantMsg(createForm(item.payload));
+          // addAssistantMsg(element);
         }
-        localStorage.setItem("messages", chatWindow.innerHTML);
 
         // After processing the last item, check for the specific message
         if (index === array.length - 1) {
@@ -182,28 +237,23 @@ document.addEventListener("DOMContentLoaded", (event) => {
         // Fade out previous content
         responseContainer.style.opacity = "0";
 
-        // Add user message to the chat window
-        const taglineElement = document.createElement("div");
-        taglineElement.classList.add("usertagline");
-        taglineElement.textContent = "You";
-        chatWindow.appendChild(taglineElement);
+        // // Add user message to the chat window
+        // chatWindow.appendChild(userTagLine);
 
-        const userWrapper = document.createElement("div");
-        userWrapper.classList.add("userwrapper");
+        // const userWrapper = document.createElement("div");
+        // userWrapper.classList.add("userwrapper");
 
-        const userImage = document.createElement("div");
-        userImage.classList.add("userimage");
-        userWrapper.appendChild(userImage);
+        // const userMessageElement = document.createElement("div");
+        // userMessageElement.classList.add("message", "user");
+        // userMessageElement.textContent = userInput;
 
-        const userMessageElement = document.createElement("div");
-        userMessageElement.classList.add("message", "user");
-        userMessageElement.textContent = userInput;
-        userWrapper.appendChild(userMessageElement);
+        addUserMsg(userInput);
+        // userWrapper.appendChild(userMessageElement);
 
-        chatWindow.appendChild(userWrapper);
+        // chatWindow.appendChild(userWrapper);
 
         // Save messages to local storage
-        localStorage.setItem("messages", chatWindow.innerHTML);
+        // localStorage.setItem("messages", chatWindow.innerHTML);
 
         // Scroll to the bottom of the chat window
         window.requestAnimationFrame(() => {
@@ -216,50 +266,12 @@ document.addEventListener("DOMContentLoaded", (event) => {
         typingIndicator.classList.remove("hidden");
         chatWindow.appendChild(typingIndicator);
 
-        interact(userInput);
+        interact({ type: "text", payload: userInput });
       }
     }
   });
 
   // Send user input to Voiceflow Dialog API
-
-  async function interact(input) {
-    // Show the typing indicator before sending the message
-    if (typingIndicator) {
-      typingIndicator.style.display = "flex";
-    }
-    // or typingIndicator.classList.remove('hidden');
-
-    let body = {
-      config: { tts: true, stripSSML: true },
-      action: { type: "text", payload: input },
-    };
-
-    // If input is #launch# > Use a launch action to the request body
-    if (input == "#launch#") {
-      body = {
-        config: { tts: true, stripSSML: true },
-        action: { type: "launch" },
-      };
-    }
-
-    fetch(`https://${voiceflowRuntime}/state/user/${uniqueId}/interact/`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: voiceflowAPIKey,
-        versionID: voiceflowVersionID,
-      },
-      body: JSON.stringify(body),
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        displayResponse(data);
-      })
-      .catch((err) => {
-        displayResponse(null);
-      });
-  }
 });
 // Function to generate a unique ID for the user
 function generateUniqueId() {
@@ -276,31 +288,74 @@ function generateUniqueId() {
   return uniqueId;
 }
 
-function handleButtonClick(event) {
-  // Log the button name as a user message
-  const userMessageElement = document.createElement("div");
+async function interact(action) {
+  // Show the typing indicator before sending the message
+  if (typingIndicator) {
+    typingIndicator.style.display = "flex";
+  }
+  // or typingIndicator.classList.remove('hidden');
 
-  const prevMessage = chatWindow.lastElementChild;
-  if (!prevMessage || !prevMessage.classList.contains("user")) {
-    const userTaglineElement = document.createElement("div");
-    userTaglineElement.classList.add("usertagline");
-    userTaglineElement.textContent = "You";
-    chatWindow.appendChild(userTaglineElement);
+  let body = {
+    config: { tts: true, stripSSML: true },
+    action: action,
+  };
+  // let body = {
+  //   config: { tts: true, stripSSML: true },
+  //   action: { type: "text", payload: input },
+  // };
+
+  // If input is #launch# > Use a launch action to the request body
+  if (action == "#launch#") {
+    body = {
+      config: { tts: true, stripSSML: true },
+      action: { type: "launch" },
+    };
   }
 
-  const userWrapper = document.createElement("div");
-  userWrapper.classList.add("userwrapper");
+  fetch(`https://${voiceflowRuntime}/state/user/${uniqueId}/interact/`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: voiceflowAPIKey,
+      versionID: voiceflowVersionID,
+    },
+    body: JSON.stringify(body),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      displayResponse(data);
+    })
+    .catch((err) => {
+      displayResponse(null);
+    });
+}
 
-  const userImage = document.createElement("div");
-  userImage.classList.add("userimage");
-  userWrapper.appendChild(userImage);
+function handleButtonClick(event) {
+  // Log the button name as a user message
+  // const userMessageElement = document.createElement("div");
 
-  userMessageElement.classList.add("message", "user");
-  userMessageElement.textContent = event.target.textContent;
-  userWrapper.appendChild(userMessageElement);
+  // const prevMessage = chatWindow.lastElementChild;
+  // if (!prevMessage || !prevMessage.classList.contains("user")) {
+  //   const userTaglineElement = document.createElement("div");
+  //   userTaglineElement.classList.add("usertagline");
+  //   userTaglineElement.textContent = "You";
+  //   chatWindow.appendChild(userTaglineElement);
+  // }
 
-  chatWindow.appendChild(userWrapper);
+  // const userWrapper = document.createElement("div");
+  // userWrapper.classList.add("userwrapper");
 
+  // const userImage = document.createElement("div");
+  // userImage.classList.add("userimage");
+  // userWrapper.appendChild(userImage);
+
+  // userMessageElement.classList.add("message", "user");
+  // userMessageElement.textContent = event.target.textContent;
+  // userWrapper.appendChild(userMessageElement);
+
+  // chatWindow.appendChild(userWrapper);
+
+  addUserMsg(event.target.textContent);
   let body = { request: { type: event.target.dataset.key } };
   event.target.parentElement.remove();
   fetch(`https://${voiceflowRuntime}/state/user/${uniqueId}/interact/`, {
@@ -321,4 +376,170 @@ function handleButtonClick(event) {
       displayResponse(null);
     });
   // Send the button label as input to the API and handle the response
+}
+
+function updateVariable(variable, value) {
+  return new Promise(async (resolve, reject) => {
+    let data = {};
+    data[variable] = value;
+    await fetch(
+      `https://${voiceflowRuntime}/state/user/${uniqueId}/variables/`,
+      {
+        method: "PATCH",
+        body: JSON.stringify(data),
+        headers: {
+          authorization: voiceflowAPIKey,
+          "content-type": "application/json",
+        },
+      }
+    );
+
+    resolve(data.conversations);
+  });
+}
+
+function addAssistantMsg(element) {
+  const assistantTagLine = document.createElement("div");
+  assistantTagLine.classList.add("assistanttagline");
+  assistantTagLine.textContent = assistantTag;
+
+  const assistantImage = document.createElement("div");
+  assistantImage.classList.add("assistantimage");
+
+  const assistantWrapper = document.createElement("div");
+  assistantWrapper.classList.add("assistantwrapper");
+  assistantWrapper.appendChild(assistantImage);
+  assistantWrapper.appendChild(element);
+
+  const assistantMsg = document.createElement("div");
+  assistantMsg.classList.add("assistantMsg");
+  assistantMsg.appendChild(assistantTagLine);
+  assistantMsg.appendChild(assistantWrapper);
+
+  chatWindow.appendChild(assistantMsg);
+  localStorage.setItem("messages", chatWindow.innerHTML);
+}
+
+function addUserMsg(userInput) {
+  const userTagLine = document.createElement("div");
+  userTagLine.classList.add("usertagline");
+  userTagLine.textContent = userTag;
+
+  const userImage = document.createElement("div");
+  userImage.classList.add("userimage");
+  const userMessageElement = document.createElement("div");
+  userMessageElement.classList.add("message", "user");
+  userMessageElement.textContent = userInput;
+
+  const userWrapper = document.createElement("div");
+  userWrapper.classList.add("userwrapper");
+  userWrapper.appendChild(userImage);
+  userWrapper.appendChild(userMessageElement);
+
+  const userMsg = document.createElement("div");
+  userMsg.classList.add("userMsg");
+  userMsg.appendChild(userTagLine);
+  userMsg.appendChild(userWrapper);
+
+  chatWindow.appendChild(userMsg);
+  localStorage.setItem("messages", chatWindow.innerHTML);
+}
+
+function createForm(dataObject) {
+  const form = document.createElement("form");
+  form.classList.add("userDetailsForm");
+
+  for (const key in dataObject) {
+    if (dataObject.hasOwnProperty(key)) {
+      const label = document.createElement("label");
+      label.textContent =
+        key.charAt(0).toUpperCase() + key.slice(1).replaceAll("_", " "); // Capitalize the first letter
+      label.classList.add("userDetailsFormLabel");
+
+      const input = document.createElement("input");
+      input.setAttribute("type", "text");
+      input.setAttribute("name", key);
+      input.setAttribute("value", dataObject[key]);
+      input.setAttribute("required", "true");
+      input.classList.add("userDetailsFormInput");
+
+      const br = document.createElement("br");
+
+      form.appendChild(label);
+      form.appendChild(input);
+      form.appendChild(br);
+    }
+  }
+  const submitButton = document.createElement("input");
+  submitButton.setAttribute("type", "submit");
+  submitButton.setAttribute("value", "Submit");
+  submitButton.classList.add("assistant", "message", "button");
+
+  submitButton.addEventListener("click", async function (event) {
+    event.preventDefault(); // Prevent default form submission
+    const isValid = form.checkValidity();
+    if (!isValid) {
+      alert("Please fill in all required fields.");
+      return;
+    }
+
+    // Disable all input fields
+    const inputFields = form.querySelectorAll("input");
+    inputFields.forEach((input) => {
+      input.setAttribute("disabled", "true");
+    });
+
+    // Remove the submit button
+    submitButton.remove();
+
+    const formData = {};
+    inputFields.forEach((input) => {
+      formData[input.name] = input.value;
+    });
+    // Make an asynchronous call to the updateData function
+    await updateData(formData);
+  });
+
+  form.appendChild(submitButton);
+  return form;
+}
+
+function updateData(formData) {
+  return new Promise(async (resolve, reject) => {
+    console.log(formData);
+    const customFields = {
+      hobbies: formData.hobbies,
+      "last-name": formData.last_name,
+      "first-name": formData.first_name,
+      speciality: formData.speciality,
+      "visa-status": formData.visa_status,
+      "current-state": formData.state,
+      "graduation-date": formData.graduation_date,
+    };
+    await window.$memberstackDom.updateMember({
+      customFields,
+    });
+    updateTextContent(customFields);
+    document.querySelector(".userDetailsForm").parentElement.parentElement.remove();
+    localStorage.setItem("messages", chatWindow.innerHTML);
+    interact({ type: "done", payload: null });
+
+    resolve("");
+  });
+}
+
+function updateTextContent(customFields) {
+  // Loop through the items in the customFields object
+  for (const key in customFields) {
+    if (customFields.hasOwnProperty(key)) {
+      // Find elements with the specified data-ms-member attribute
+      const elements = document.querySelectorAll(`[data-ms-member="${key}"]`);
+
+      // Update the textContent of each matching element
+      elements.forEach((element) => {
+        element.textContent = customFields[key];
+      });
+    }
+  }
+
 }
